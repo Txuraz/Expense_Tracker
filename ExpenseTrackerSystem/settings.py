9177ca415e600687b3d5c637112019755438f473
Txuraz <52666637+Txuraz@users.noreply.github.com>
2023-10-11 13:56:10 +0545

    host changed to allow all

--- ExpenseTrackerSystem/settings.py
+++ ExpenseTrackerSystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^9_!ue^2n!v5957o_=jnm0%)m+ni9x(49i0edjmpxy6^mmyg*o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
